93014999c878e1b7e51d9ec10bfdb40f7701d696
jiwon1217 <koagjw@gmail.com>
2023-07-09 00:48:39 +0900

    refactor(OutputView): createWinningsMap 메서드 사용하여 winnings 초기화, 코드 포맷팅, import 순서 변경, printErrorMessage 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,16 +1,17 @@
 package lotto.view;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import lotto.domain.Lotto;
 import lotto.domain.LottoCount;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.domain.result.Rank;
 import lotto.domain.result.Result;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private static final String NEW_LINE = System.lineSeparator();
@@ -23,40 +24,42 @@ public class OutputView {
     private static final String TOTAL_YIELD = "총 수익률은 %.1f%%입니다.";
     private static final int ONE_HUNDRED = 100;
 
-    private static final Map<Integer, String> winnings = new LinkedHashMap<>() {{
-        put(2_000_000_000, "2,000,000,000");
-        put(30_000_000, "30,000,000");
-        put(1_500_000, "1,500,000");
-        put(50_000, "50,000");
-        put(5_000, "5,000");
-    }};
+    private static final Map<Integer, String> winnings = createWinningsMap();
+
+    private static Map<Integer, String> createWinningsMap() {
+        Map<Integer, String> map = new LinkedHashMap<>();
+        map.put(2_000_000_000, "2,000,000,000");
+        map.put(30_000_000, "30,000,000");
+        map.put(1_500_000, "1,500,000");
+        map.put(50_000, "50,000");
+        map.put(5_000, "5,000");
+        return map;
+    }
 
     public static void printLottoCount(LottoCount lottoCount) {
-        System.out.print(NEW_LINE);
+        System.out.println(NEW_LINE);
         System.out.printf(NUMBER_OF_PURCHASED_LOTTOS, lottoCount.getLottoCount());
-        System.out.print(NEW_LINE);
+        System.out.println(NEW_LINE);
     }
 
     public static void printPurchasedLottoNumbers(Lottos lottos) {
         for (Lotto lotto : lottos.getLottos()) {
             List<Integer> numbers = new ArrayList<>(lotto.get());
-
             Collections.sort(numbers);
             System.out.println(numbers);
         }
-        System.out.print(NEW_LINE);
+        System.out.println(NEW_LINE);
     }
 
     public static void printStatistic(Result result) {
-        System.out.printf(NEW_LINE);
+        System.out.println(NEW_LINE);
         System.out.println(STATISTIC_HEADER);
         System.out.println(STATISTIC_BORDER);
 
         for (Rank rank : Rank.getRankWithoutMiss()) {
             System.out.printf(STATISTIC_BODY, rank.getMatchCount(), getMatchBonusBall(rank),
-                    winnings.get(rank.getWinnings()),
-                    result.getRankCount(rank));
-            System.out.printf(NEW_LINE);
+                    winnings.get(rank.getWinnings()), result.getRankCount(rank));
+            System.out.println();
         }
     }
 
@@ -71,5 +74,9 @@ public class OutputView {
         double winnings = result.getWinnings();
         double yield = (winnings / money.getMoney()) * ONE_HUNDRED;
         System.out.printf(TOTAL_YIELD, yield);
+    }
+
+    public static void printErrorMessage(String message){
+        System.out.println(message);
     }
 }
