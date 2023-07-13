c7f6b15e3568ef0354bc11866fd73a65e0dfe13b
jiwon1217 <koagjw@gmail.com>
2023-07-13 21:27:50 +0900

    refactor(LottoCount): DEFAULT_UNIT 자료형 Integer로 변경, calculateLottoCount 메서드 추가

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -1,14 +1,19 @@
 package lotto.domain;
 
 public class LottoCount {
-    private static final String DEFAULT_UNIT = "1000";
+
+    private static final Integer DEFAULT_UNIT = 1000;
     private final int lottoCount;
 
     public LottoCount(Money money) {
-        lottoCount = money.calculateLottoCount(new Money(DEFAULT_UNIT));
+        lottoCount = calculateLottoCount(money, new Money(DEFAULT_UNIT));
     }
 
     public int getLottoCount() {
         return lottoCount;
+    }
+
+    public int calculateLottoCount(Money money, Money unit) {
+        return money.getMoney() / unit.getMoney();
     }
 }
