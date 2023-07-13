089c5b73e14439f8063b3f5fd6fdeeeb3d596898
jiwon1217 <koagjw@gmail.com>
2023-07-13 22:21:10 +0900

    refactor(MoneyTest): 예외처리 test 세분화, 정상 작동 테스트 추가, 메서드 이름 변경

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,15 +1,45 @@
 package lotto.domain;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import lotto.exception.MoneyException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 class MoneyTest {
+
+    @DisplayName("금액_생성_테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"ㄱㄴㄷ", "abc", "-1", "-10", "10", "100"})
-    void 금액은_1000원_단위로_나누어_떨어지는_양수만_가능하다(String money) {
-        assertThatThrownBy(() -> new Money(money))
-                .isInstanceOf(IllegalArgumentException.class);
+    @ValueSource(ints = {1000, 2000, 3000})
+    void generateMoneyTest(Integer inputMoney) {
+        Money money = new Money(inputMoney);
+        assertThat(money.getMoney()).isEqualTo(inputMoney);
     }
+
+    @DisplayName("금액은_숫자만_가능하다")
+    @ParameterizedTest
+    @ValueSource(strings = {"ㄱㄴㄷ", "abc", "aㄱb"})
+    void moneyTypeTest(String money) {
+        assertThatThrownBy(() -> MoneyException.validateMoneyType(money))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액은_양수만_가능하다")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1000", "-2000", "-3000"})
+    void positiveNumberTest(String money) {
+        assertThatThrownBy(() -> MoneyException.validatePositiveNumber(money))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액은_1000원_단위로_나누어_떨어지는_금액만_가능하다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "10", "100"})
+    void divideByThousandUnitTest(String money) {
+        assertThatThrownBy(() -> MoneyException.validateDivideByThousandUnit(money))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
