e8714d3d1d0ae13764f783c9c538cf4eb16e2d50
jiwon1217 <koagjw@gmail.com>
2023-07-13 21:30:30 +0900

    refactor(InputView): inputMoney() 메서드에 예외처리 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,16 +1,29 @@
 package lotto.view;
 
+import static lotto.exception.MoneyException.validateDivideByThousand;
+import static lotto.exception.MoneyException.validateMoneyType;
+import static lotto.exception.MoneyException.validatePositive;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
     private static final String NEW_LINE = System.lineSeparator();
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static String inputMoney() {
+    public static Integer inputMoney() {
         System.out.println(INPUT_MONEY);
-        return Console.readLine();
+        String money = Console.readLine();
+        validateMoney(money);
+        return Integer.parseInt(money);
+    }
+
+    private static void validateMoney(String money) {
+        validateMoneyType(money);
+        validatePositive(money);
+        validateDivideByThousand(money);
     }
 
     public static String inputWinningLottoNumbers() {
