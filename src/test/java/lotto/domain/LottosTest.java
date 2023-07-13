df626eb8df5eedd9ccdee609d04047d9df38b6cd
jiwon1217 <koagjw@gmail.com>
2023-07-13 22:25:09 +0900

    refactor(LottosTest): values strings -> ints 로 변경, 메서드 이름 변경

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,15 +1,18 @@
 package lotto.domain;
 
 import lotto.util.LottoNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class LottosTest {
+
+    @DisplayName("로또는_구입_금액에_해당하는_만큼_발행된다")
     @ParameterizedTest
-    @ValueSource(strings = {"1000", "2000", "3000"})
-    void 로또는_구입_금액에_해당하는_만큼_발행된다(String inputMoney) {
+    @ValueSource(ints = {1000, 2000, 3000})
+    void generateLottosTest(Integer inputMoney) {
         Money money = new Money(inputMoney);
         LottoCount lottoCount = new LottoCount(money);
 
