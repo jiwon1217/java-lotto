d022df03ad9a62a86d33b2d42c6f1ff84eefd6b2
jiwon1217 <koagjw@gmail.com>
2023-07-13 21:25:55 +0900

    refactor(Money): 생성자에 포함된 예외처리 로직 제거, calculateLottoCount 메서드 위치 Money -> LottoCount로 변경

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,24 +1,13 @@
 package lotto.domain;
 
-import static lotto.exception.MoneyException.validateDivideByThousand;
-import static lotto.exception.MoneyException.validatePositive;
-import static lotto.exception.MoneyException.validateMoneyType;
-
 public class Money {
     private final int money;
 
-    public Money(String money) {
-        validateMoneyType(money);
-        validatePositive(money);
-        validateDivideByThousand(money);
-        this.money = Integer.parseInt(money);
+    public Money(Integer money) {
+        this.money = money;
     }
 
     public int getMoney() {
         return money;
-    }
-
-    public int calculateLottoCount(Money unit) {
-        return this.money / unit.money;
     }
 }
