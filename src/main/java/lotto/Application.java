9470b741028de0e04d9d58dd9cee80d8e72e87b7
jiwon1217 <koagjw@gmail.com>
2023-07-28 20:59:25 +0900

    refactor(Application): 연관 로직 메서드로 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,33 +1,53 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoCount;
-import lotto.util.LottoNumberGenerator;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
-import lotto.domain.result.Result;
 import lotto.domain.WinningLotto;
+import lotto.domain.result.Result;
+import lotto.exception.BonusNumberException;
+import lotto.exception.WinningLottoException;
+import lotto.util.LottoConverter;
+import lotto.util.LottoNumberGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
+
     public static void main(String[] args) {
         try {
             Money money = new Money(InputView.inputMoney());
             LottoCount lottoCount = new LottoCount(money);
             OutputView.printLottoCount(lottoCount);
 
-            Lottos lottos = new LottoNumberGenerator().generateLottos(lottoCount);
+            Lottos lottos = LottoNumberGenerator.generateLottos(lottoCount);
             OutputView.printPurchasedLottoNumbers(lottos);
 
-            String winningLottoNumbers = InputView.inputWinningLottoNumbers();
-            Integer bonusNumber = InputView.inputBonusNumber();
-            WinningLotto winningLotto = new WinningLotto(winningLottoNumbers, bonusNumber);
-
+            WinningLotto winningLotto = createWinningLotto();
             Result result = new Result(lottos, winningLotto);
-            OutputView.printStatistic(result);
-            OutputView.printYield(result, money);
+
+            printResult(result, money);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            OutputView.printErrorMessage(e.getMessage());
         }
+    }
+
+    private static WinningLotto createWinningLotto() {
+        String winningLottoNumbers = InputView.inputWinningLottoNumbers();
+        WinningLottoException.validateInputPattern(winningLottoNumbers);
+
+        Lotto winningLotto = LottoConverter.convertToLotto(winningLottoNumbers);
+
+        int bonusNumber = InputView.inputBonusNumber();
+        BonusNumberException.validateNumberRange(bonusNumber);
+        WinningLottoException.validateDuplicateCheckWinningLottoAndBonusNumber(winningLotto, bonusNumber);
+
+        return new WinningLotto(winningLotto, bonusNumber);
+    }
+
+    private static void printResult(Result result, Money money) {
+        OutputView.printStatistic(result);
+        OutputView.printYield(result, money);
     }
 }
