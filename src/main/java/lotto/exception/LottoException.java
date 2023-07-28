3b5bdaa73dba8b00e221ae40a2ee9a8e8cf072d6
jiwon1217 <koagjw@gmail.com>
2023-07-28 20:47:59 +0900

    refactor(LottoException): validateLotto 메서드 안에서 다른 예외처리 메서드를 호출하도록 변경

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -3,6 +3,7 @@ package lotto.exception;
 import java.util.List;
 
 public class LottoException {
+
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 45;
     private static final int LOTTO_LENGTH = 6;
@@ -10,13 +11,19 @@ public class LottoException {
     public static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 번호의 범위는 1 ~ 45까지 입니다.";
     public static final String DUPLICATE_LOTTO_NUMBER = "[ERROR] 로또의 6개의 수는 중복될 수 없습니다.";
 
-    public static void validateLottoSize(List<Integer> numbers) {
+    public static void validateLotto(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        validateNumberRange(numbers);
+        validateDuplicateNumber(numbers);
+    }
+
+    private static void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_LENGTH) {
             throw new IllegalArgumentException(INVALID_LOTTO_SIZE);
         }
     }
 
-    public static void validateNumberRange(List<Integer> numbers) {
+    private static void validateNumberRange(List<Integer> numbers) {
         for (int number : numbers) {
             if (number < MIN_RANGE || number > MAX_RANGE) {
                 throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
@@ -24,7 +31,7 @@ public class LottoException {
         }
     }
 
-    public static void validateDuplicateNumber(List<Integer> numbers) {
+    private static void validateDuplicateNumber(List<Integer> numbers) {
         int uniqueNumberCount = (int) numbers.stream()
             .distinct()
             .count();
