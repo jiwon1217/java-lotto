a45a2c872253cac3c2d387c3a41ab69efbdce53f
jiwon1217 <koagjw@gmail.com>
2023-07-28 20:17:58 +0900

    refactor(WinningLotto): 생성자에 불필요한 로직 삭제, string -> List<Integer> 변환 메서드 유틸 클래스로 분리

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,21 +1,11 @@
 package lotto.domain;
 
-import static lotto.exception.WinningLottoException.validateBonusNumber;
-import static lotto.exception.WinningLottoException.validateInputPattern;
-import static lotto.exception.WinningLottoException.validateNumberRange;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class WinningLotto {
     private final Lotto winningLotto;
     private final int bonusNumber;
 
-    public WinningLotto(String winningLotto, int bonusNumber) {
-        validateInputPattern(winningLotto);
-        this.winningLotto = convert(winningLotto);
-        validateBonusNumber(this.winningLotto, bonusNumber);
-        validateNumberRange(bonusNumber);
+    public WinningLotto(Lotto winningLotto, int bonusNumber) {
+        this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
 
@@ -25,12 +15,6 @@ public class WinningLotto {
 
     public int getBonusNumber() {
         return bonusNumber;
-    }
-
-    private Lotto convert(String winningLotto) {
-        return new Lotto(Arrays.stream(winningLotto.split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList()));
     }
 
     public boolean isContain(int number) {
